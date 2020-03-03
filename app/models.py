79622e1a27273fa73e0a50e30d953d26c1ef6a3e
Chris <chrishaining@yahoo.co.uk>
2020-03-03 12:30:05 +0000

    user can see stats for steps: best day, total steps, five number summary, variance, standard deviation; can also add new entry

--- app/models.py
+++ app/models.py
@@ -34,8 +34,8 @@ class User(db.Model):
         iqr = stats.iqr(steps_list, interpolation = 'midpoint') 
         return "The IQR is {}.".format(iqr)
 
-    # method(s) to find the five-number summary 
-    def create_five_figure_summary(self):
+    # method(s) to find the five-number summary. Extension: return the values in a list, so I can separate them in rendering (e.g. to put into a table)
+    def create_five_figure_summary_for_steps(self):
         steps_list = [entry.steps for entry in self.journal_entries]
         percentiles = np.percentile(steps_list, [25, 50, 75]) # creates LQ, median and UQ. These could be done as separate variables - I just wanted to try it this way
         min, max = np.min(steps_list), np.max(steps_list)
@@ -48,6 +48,40 @@ class User(db.Model):
         steps_list = [entry.steps for entry in self.journal_entries]
         total_steps = np.sum(steps_list)
         return "Total steps: {}".format(total_steps)
+
+    # find variance in steps
+    def find_steps_variance(self):
+        steps_list = [entry.steps for entry in self.journal_entries]
+        variance = np.var(steps_list)
+        return "Variance of steps: {}".format(variance)
+
+    # variance is giving what seems a crazy answer (9,384,542.5), so I want to try to work it out step by step. 
+    # Find the mean 
+    def find_mean_steps(self):
+        steps_list = [entry.steps for entry in self.journal_entries]
+        mean = np.mean(steps_list)
+        return "Mean: {}".format(mean)
+
+    # mean is working. So, for each number: subtract the Mean and square the result (the squared difference). Then work out the average of those squared differences. Well, it works out the same. I guess I just hadn't thought enough about what variance is.
+    def manual_steps_variance(self):
+        steps_list = [entry.steps for entry in self.journal_entries]
+        mean = np.mean(steps_list)
+        deviations = []
+        for entry in steps_list:
+            deviation = entry - mean
+            deviations.append(deviation)
+        deviations_sum = 0
+        for deviation in deviations:
+            squared_deviation = deviation * deviation 
+            deviations_sum += squared_deviation
+        variance = deviations_sum / len(steps_list)
+        return "Manually worked out variance is: {}".format(variance)
+
+    # find standard deviation in steps. (using numpy: scipy gives a more accurate answer, but I want to use variation as well, and I don't understand scipy's variation. So, for consistency, I'll stick to numpy). Use two decimal places.
+    def find_steps_standard_deviation(self):
+        steps_list = [entry.steps for entry in self.journal_entries]
+        sd = round(np.std(steps_list), 2)
+        return "Standard deviation of steps: {}".format(sd)
 
     # count total number of times the user has done a given activity (e.g. number of gym visits)
 
--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,23 @@ def index():
     greeting = "WooooooW"
     user = User.query.get(1)
     entries = JournalEntry.query.all()
-    best_day = user.find_best_day()
-    return render_template('index.html', title="Home", greeting=greeting, user=user, entries=entries, best_day=best_day)
+    steps_best_day = user.find_best_day()
+    total_steps = user.count_total_steps()
+    steps_summary = user.create_five_figure_summary_for_steps()
+    steps_variance = user.find_steps_variance()
+    steps_standard_deviation = user.find_steps_standard_deviation()
+    return render_template('index.html', title="Home", greeting=greeting, user=user, entries=entries, steps_best_day=steps_best_day, total_steps=total_steps, steps_summary=steps_summary, steps_variance=steps_variance, steps_standard_deviation=steps_standard_deviation)
 
 @app.route('/manage')
 def manage_entries():
     return render_template('manage.html', title="Manage Your Entries")
+
+@app.route('/manage', methods=['POST'])
+def add_entry():
+    # date = request.form['date']
+    steps = request.form['steps']
+    description = request.form['description']
+    newEntry = JournalEntry(steps=steps, description=description)
+    db.session.add(newEntry)
+    db.session.commit()
+    return redirect('/manage')
